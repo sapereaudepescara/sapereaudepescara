ebfa06d5cd739d3bc4a40ddf41944d5917da0c66
sapereaudepescara <128394794+sapereaudepescara@users.noreply.github.com>
2023-03-30 20:20:42 +0200

    Delete social.js

--- social.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    let myiFrame = document.getElementById("instagram-embed-0");
-    let doc = myiFrame.contentDocument;
-    doc.body.innerHTML = doc.body.innerHTML + '<style>.Header, .EmbeddedMedia{display: none !important;}</style>';
-}
