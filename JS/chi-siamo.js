ec89440076a874135c74f6155878b1f11b9ccb4f
sapereaudepescara <128394794+sapereaudepescara@users.noreply.github.com>
2023-04-10 23:07:57 +0200

    Add files via upload

--- JS/chi-siamo.js
+++ JS/chi-siamo.js
@@ -1,57 +1,58 @@
-p1 = document.querySelector("#p1");
-p2 = document.querySelector("#p2");
-p3 = document.querySelector("#p3");
-p4 = document.querySelector("#p4");
-p5 = document.querySelector("#p5");
-p6 = document.querySelector("#p6");
-p7 = document.querySelector("#p7");
-p8 = document.querySelector("#p8");
-p9 = document.querySelector("#p9");
-p10 = document.querySelector("#p10");
+p1 = document.getElementById("p1");
+p2 = document.getElementById("p2");
+p3 = document.getElementById("p3");
+p4 = document.getElementById("p4");
+p5 = document.getElementById("p5");
+p6 = document.getElementById("p6");
+p7 = document.getElementById("p7");
+p8 = document.getElementById("p8");
+p9 = document.getElementById("p9");
+p10 = document.getElementById("p10");
 
-pa1 = document.querySelector("#pa1");
-pa2 = document.querySelector("#pa2");
-pa3 = document.querySelector("#pa3");
-pa4 = document.querySelector("#pa4");
-pa5 = document.querySelector("#pa5");
-pa6 = document.querySelector("#pa6");
-pa7 = document.querySelector("#pa7");
-pa8 = document.querySelector("#pa8");
-pa9 = document.querySelector("#pa9");
-pa10 = document.querySelector("#pa10");
+pa1 = document.getElementById("pa1");
+pa1m = document.querySelector("#pa1 img");
+pa2 = document.getElementById("pa2");
+pa3 = document.getElementById("pa3");
+pa4 = document.getElementById("pa4");
+pa5 = document.getElementById("pa5");
+pa6 = document.getElementById("pa6");
+pa7 = document.getElementById("pa7");
+pa8 = document.getElementById("pa8");
+pa9 = document.getElementById("pa9");
+pa10 = document.getElementById("pa10");
+
+ics = document.querySelector(".ics")
+
+var aperto = false;
 
 
-/* function profiloApprofondito1Aprire(){
-    if(pa1.classList.contains("profiloChiuso")){
-        p1.addEventListener("click", ()=>{
-            pa1.classList.add("profiloAperto")
-            pa1.classList.remove("profiloChiuso")
-        })
-    } else {
-        return 0;
-    }
-}
-
-function profiloApprofondito1Chiusdere(){
-    if(pa1.classList.contains("profiloAperto")){
-        pa1.addEventListener("click", ()=>{
-            pa1.classList.remove("profiloAperto")
-            pa1.classList.add("profiloChiuso")
-        })
-    } else {
-        return 0;
-    }
-}
-
-profiloApprofondito1Aprire() */
-if(pa1.classList.contains("profiloChiuso")){
-    p1.addEventListener("click", ()=>{
-        pa1.classList.add("profiloAperto")
-        pa1.classList.remove("profiloChiuso")
-    })
-} else if (pa1.classList.contains("profiloAperto")){
-    p1.addEventListener("click", ()=>{
-        pa1.classList.add("profiloAperto")
-        pa1.classList.remove("profiloChiuso")
-    })
-}
+p1.addEventListener("click", ()=>{
+    pa1.classList.toggle("profiloAperto")
+})
+p2.addEventListener("click", ()=>{
+    pa2.classList.toggle("profiloAperto")
+})
+p3.addEventListener("click", ()=>{
+    pa3.classList.toggle("profiloAperto")
+})
+p4.addEventListener("click", ()=>{
+    pa4.classList.toggle("profiloAperto")
+})
+p5.addEventListener("click", ()=>{
+    pa5.classList.toggle("profiloAperto")
+})
+p6.addEventListener("click", ()=>{
+    pa6.classList.toggle("profiloAperto")
+})
+p7.addEventListener("click", ()=>{
+    pa7.classList.toggle("profiloAperto")
+})
+p8.addEventListener("click", ()=>{
+    pa8.classList.toggle("profiloAperto")
+})
+p9.addEventListener("click", ()=>{
+    pa9.classList.toggle("profiloAperto")
+})
+p10.addEventListener("click", ()=>{
+    pa10.classList.toggle("profiloAperto")
+})
--- JS/links.js
+++ JS/links.js
@@ -1,18 +1,36 @@
 const imgLinks = document.querySelectorAll(".social img")
+const social = document.querySelector(".social")
 const links = document.querySelectorAll(".links")
+const socialMedia = document.querySelector(".socialMedia")
 
-imgLinks[0].addEventListener("click", ()=>{
-    if(imgLinks[0].classList.contains("ruotareLinks")){
-        imgLinks[0].classList.remove("ruotareLinks")
-        imgLinks[0].classList.add("ruotareIndietroLinks")
-        for(i=0; i<links.length; i++){
-            links[i].classList.remove("ciao")
+if (window.matchMedia("(max-width: 1200px)")) {
+    imgLinks[0].addEventListener("click", ()=>{
+        if(imgLinks[0].classList.contains("ruotareLinks")){
+            imgLinks[0].classList.remove("ruotareLinks")
+            imgLinks[0].classList.add("ruotareIndietroLinks")
+            /* social.style.border = "1px solid #727272"; */
+            socialMedia.classList.remove("socialMediaAperto")
+        } else {
+            imgLinks[0].classList.add("ruotareLinks")
+            imgLinks[0].classList.remove("ruotareIndietroLinks")
+            /* social.style.border = "unset"; */
+            socialMedia.classList.add("socialMediaAperto")
         }
-    } else {
-        imgLinks[0].classList.add("ruotareLinks")
-        for(i=0; i<links.length; i++){
-            links[i].classList.add("ciao")
+    })
+} else {
+    imgLinks[0].addEventListener("click", ()=>{
+        if(imgLinks[0].classList.contains("ruotareLinks")){
+            imgLinks[0].classList.remove("ruotareLinks")
+            imgLinks[0].classList.add("ruotareIndietroLinks")
+            for(i=0; i<links.length; i++){
+                links[i].classList.remove("ciao")
+            }
+        } else {
+            imgLinks[0].classList.add("ruotareLinks")
+            for(i=0; i<links.length; i++){
+                links[i].classList.add("ciao")
+            }
+            imgLinks[0].classList.remove("ruotareIndietroLinks")
         }
-        imgLinks[0].classList.remove("ruotareIndietroLinks")
-    }
-})
+    })
+}
